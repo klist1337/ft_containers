cda5b5d09b4e8eb1a20c92090d994a5b25dd0812
Ehouman dominique Assofi <eassofi@student.1337.ma>
2023-01-04 04:39:11 +0100

    avltree done

--- avl_tree.hpp
+++ avl_tree.hpp
@@ -1,5 +1,6 @@
 #ifndef AVL_TREE_HPP
 #define AVL_TREE_HPP
+#include <algorithm>
 namespace ft
 {
   template <class T>
@@ -11,7 +12,7 @@ namespace ft
     Node *right;
     int   balance_factor;
     int   height;
-    Node() : parent(nullptr), left(nullptr), right(nullptr)
+    Node() : parent(nullptr), left(nullptr), right(nullptr), height(1)
     {} 
     Node(const T& value) : value(value)
     {}
@@ -55,7 +56,13 @@ namespace ft
     size_type size() const { return _size;}
     bool empty() const { return (!size) ? true : false ;}
     size_type max_size() const { return _alloc.max_size;}
-    nodePtr Get_end const(this->_end);
+    nodePtr Get_end() const {return this->_end;}
+    int height(nodePtr node)
+    {
+      if (node == nullptr) 
+        return 0;
+      return node->height;
+    }
     nodePtr Get_min(nodePtr node = nullptr) const
     {
       if (!node)
@@ -73,7 +80,7 @@ namespace ft
       while (node && node->right)
         node = node->right;
     }
-    nodePtr lowerBound(const value_type& value)
+    nodePtr lowerbound(const value_type& value)
     {
       nodePtr node = Get_min();
       while (node != _end)
@@ -84,7 +91,7 @@ namespace ft
       }
       return(_end);
     }
-    nodePtr Upper_Bound(const value_type& value)
+    nodePtr upperbound(const value_type& value)
     {
       nodePtr node = Get_min();
       while (node != _end)
@@ -95,7 +102,165 @@ namespace ft
       }
       return(_end);
     }
+    void swap(Avltree& tree)
+    {
+      nodePtr root = tree._root;
+      nodePtr end = tree._end;
+      allocator_type alloc = tree._alloc;
+      key_compare cmp = tree._cmp;
+      size_type size = tree._size;
 
+      tree._root = this->_root;
+      tree._end = this->_end;
+      tree._alloc = this->_alloc;
+      tree._cmp = this->_cmp;
+      tree._size = this->_size;
+
+      this->_root = root;
+      this->_end = end;
+      this->_alloc = alloc;
+      this->_cmp = cmp;
+      this->_size = size;
+    }
+    nodePtr find(const value_type& value)
+    {
+      return find(_root, value);
+    }
+    nodePtr find(nodePtr& node, const value_type& value)
+    {
+      if (!node)
+        return (nullptr);
+      if (node->value.first == value.first)
+        return (node);
+      if (_cmp(node->value.first, value.first))
+        return (find(node->right, value));
+      return(find(node->left, value));
+    }
+    void insert(const value_type& value)
+    {
+      _root = insertNode(_root, value);
+    }
+    void erase(const value_type& value)
+    {
+      _root = eraseNode(_root, value);
+    }
+    nodePtr insertNode(nodePtr node, const value_type& value)
+    {
+      //find the correct position to insert the value;
+      if (node == nullptr)
+      {
+        _alloc.allocate(1);
+        _alloc.construct(_end, value);
+        _size++;
+        return _end;
+      }
+      if (_cmp(node->value.first, value.first))
+        node = insertNode(node->right, value);
+      else if (_cmp(value.first , node->value.first))
+        node = insertNode(node->left, value);
+      node->height = 1 + std::max(height(node->right), height(node->left));
+      node->balance_factor = (height(node->left) - height(node->right));
+      if (node->balance_factor > 1)
+      {
+        if (_cmp(node->left->value.first, value.first))
+          return (right_rotation(node));
+        else
+        {
+          node->left = left_rotation(node->left);
+          return (right_rotation(node->right));
+        }
+      }
+      else if (node->balance_factor < -1)
+      {
+        if (_cmp(value.first, node->right->value.first))
+          return (left_rotation(node));
+        else
+        {
+          node->right = right_rotation(node->right);
+          return(left_rotation(node));
+        }
+      }
+    }
+    nodePtr eraseNode(nodePtr node, const value_type)
+    {
+      if (node == nullptr)
+        return (nullptr);
+      else if (_cmp(value.first, node->value.first))
+        eraseNode(node->left, value);
+      else if (_cmp(node->value.first, value.first))
+        eraseNode(node->right, value);
+      else
+      {
+        nodePtr *r = node->right;
+        nodePtr *l = node->left;
+        if (node->right == nullptr)
+        {
+          _alloc.deallocate(node);
+          node = l;
+        }
+        else if (node->left = nullptr)
+        {
+          _alloc.deallocate(node);
+          node = r;
+        }
+        else
+        {
+           while (node->left) 
+            r = r->left;
+           node->value = r->value;
+           node->right = eraseNode(node->right, node->value);
+        }
+      }
+      if (!node)
+        return node;
+      node->height = 1 + std::max(height(node->right), height(node->height));
+      node->balance_factor = height(node->left) - height(node->right);
+      if (node->balance_factor > 1)
+      {
+        if (height(node->left) >= height(node->right))
+          return (right_rotation(node));
+        else
+        {
+          node->left = left_rotation(node->left);
+          return (right_rotation(node));
+        }
+      }
+      else if (node->balance_factor < -1)
+      {
+        if (height(node->right) >= height(node->left))
+          return (left_rotation(node))
+        else
+        {
+          node->right = right_rotation(node->right);
+          return (left_rotation(node));
+        }
+      }
+      return  node;
+    }
+    nodePtr right_rotation(nodePtr node)
+    {
+      nodePtr newhead = node->left;
+      node->left = newhead->right;
+      newhead->right = node;
+      node->height = 1 + std::max(height(node->right), height(node->left));
+      newhead->height = 1 + std::max(height(newhead->right), height(newhead->left));
+      return newhead;
+    }
+    nodePtr left_rotation(nodePtr node)
+    {
+      nodePtr newhead = node->right;
+      node->right = newhead->left;
+      newhead->left = node;
+      node->height = 1 + std::max(height(node->right), height(node->left));
+      newhead->height = 1 + std::max(height(newhead->right), height(newhead->left));
+      return  newhead;
+    }
+    void inorder_util(nodePtr node)
+    {
+      inorder_util(node->left);
+      std::cout << node->value << " ";
+      inorder_util(node->right);
+    }
   };
 
 }
